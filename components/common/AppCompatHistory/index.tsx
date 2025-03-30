a68e9dcded4c73de139a3ff814dd805df936ab42
nolbo <jspark7016@gmail.com>
2025-03-30 13:01:43 +0900

    design: improve table and compatibility history

--- components/common/AppCompatHistory/index.tsx
+++ components/common/AppCompatHistory/index.tsx
@@ -9,7 +9,7 @@ export default function AppCompatibilityHistory({ history }: IAppCompatibilityHi
     return (
         <div className={
             "relative flex flex-col gap-[12px] [&_dt]:relative [&_dd]:relative " +
-            "before:content-[''] before:absolute before:z-[-1] before:top-[calc(1rem/2)] before:left-[calc(1rem/2-1px)] before:w-[2px] before:h-[calc(100%-1rem)] before:bg-gray " +
+            "before:content-[''] before:absolute before:z-[-1] before:top-[calc(1rem/2)] before:left-[calc(1rem/2-1px)] before:w-[2px] before:h-[calc(100%-1rem)] before:bg-[theme(borderColor.default)] " +
             "[&>:last-child>:last-child:before]:content-[''] [&>:last-child>:last-child:before]:absolute [&>:last-child>:last-child:before]:z-[-1] [&>:last-child>:last-child:before]:top-[calc(((theme(fontSize.sm)*1.75-1rem)/2)+.5rem)] [&>:last-child>:last-child:before]:left-[calc(1rem/2-2px)] [&>:last-child>:last-child:before]:w-[4px] [&>:last-child>:last-child:before]:h-[calc(100%-1rem)] [&>:last-child>:last-child:before]:bg-default"
         }>
             {
